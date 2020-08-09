0a4aaa70035b29cdc132f9a24c6836bd631fa805
dataXcode <smartvision789@gmail.com>
2020-08-09 15:31:48 +0300

    A1

--- exercises/test_A1.py
+++ exercises/test_A1.py
@@ -2,9 +2,9 @@ def test():
     # Test
     assert("# My first code" in __solution__ or "#My first code" in __solution__ or "# my first code" in __solution__ or "#my first code" in __solution__
            or "# My First Code" in __solution__ or "#My First Code" in __solution__ or "# MY FIRST CODE" in __solution__ or "#MY FIRST CODE" in __solution__ 
-           ), "اجابة خاطئة: لم تقم بكتابة الملاحظة بشكل الصحيح"
+           ), "اجابة خاطئة: لم تقم بكتابة الملاحظة بشكل صحيح"
 
     assert("'I love python'" in __solution__ or "'I Love Python'" in __solution__ or "'i love python'" in __solution__ or "'I LOVE PYTHON'" in __solution__
            or '"I love python"' in __solution__ or '"I Love Python"' in __solution__ or '"i love python"' in __solution__ or '"I LOVE PYTHON"' in __solution__
-           ), "اجابة خاطئة: لم تقم بطباعة الجملة المطلوبة بشكل الصحيح"
+           ), "اجابة خاطئة: لم تقم بطباعة الجملة المطلوبة بشكل صحيح"
     __msg__.good("اجابة صحيحة. احسنت")
