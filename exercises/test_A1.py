592d1a438645de793341f6e93536d9d8658deaf5
dataXcode <smartvision789@gmail.com>
2020-08-09 15:23:50 +0300

    A1

--- exercises/test_A1.py
+++ exercises/test_A1.py
@@ -1,7 +1,10 @@
-assert("# My first code" in __solution__ or "#My first code" in __solution__ or "# my first code" in __solution__ or "#my first code" in __solution__
-       or "# My First Code" in __solution__ or "#My First Code" in __solution__ or "# MY FIRST CODE" in __solution__ or "#MY FIRST CODE" in __solution__ 
-       ), "اجابة خاطئة: لم تقم بكتابة الملاحظة بشكل الصحيح"
+def test():
+    # Test
+    assert("# My first code" in __solution__ or "#My first code" in __solution__ or "# my first code" in __solution__ or "#my first code" in __solution__
+           or "# My First Code" in __solution__ or "#My First Code" in __solution__ or "# MY FIRST CODE" in __solution__ or "#MY FIRST CODE" in __solution__ 
+           ), "اجابة خاطئة: لم تقم بكتابة الملاحظة بشكل الصحيح"
 
-assert("'I love python'" in __solution__ or "'I Love Python'" in __solution__ or "'i love python'" in __solution__ or "'I LOVE PYTHON'" in __solution__
-       or '"I love python"' in __solution__ or '"I Love Python"' in __solution__ or '"i love python"' in __solution__ or '"I LOVE PYTHON"' in __solution__
-       ), "اجابة خاطئة: لم تقم بطباعة الجملة بشكل الصحيح"
+    assert("'I love python'" in __solution__ or "'I Love Python'" in __solution__ or "'i love python'" in __solution__ or "'I LOVE PYTHON'" in __solution__
+           or '"I love python"' in __solution__ or '"I Love Python"' in __solution__ or '"i love python"' in __solution__ or '"I LOVE PYTHON"' in __solution__
+           ), "اجابة خاطئة: لم تقم بطباعة الجملة المطلوبة بشكل الصحيح"
+    __msg__.good("اجابة صحيحة. احسنت")
