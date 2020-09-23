6800f1bdc6d2908636cc37b941a81d4a621efdd1
dataXcode <smartvision789@gmail.com>
2020-09-23 21:02:59 +0300

    B6

---
+++ exercises/exc_B6.py
@@ -0,0 +1,16 @@
+# Areas list
+areas = [ "hallway", 14.35,
+          "kitchen", 15.0, 
+          "living room", 19.0,
+          "bedroom", 12.5,
+          "bathroom", 8.75 ]
+
+# Use slicing to create downstairs
+downstairs = __________
+
+# Use slicing to create upstairs
+upstairs = __________
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
