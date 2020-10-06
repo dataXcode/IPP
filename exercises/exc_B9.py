adbfe0a25a3149a1585715f65a877a4c68216ed9
dataXcode <smartvision789@gmail.com>
2020-10-06 13:34:19 +0300

    B9

---
+++ exercises/exc_B9.py
@@ -0,0 +1,14 @@
+# All areas list
+all_areas = [ "hallway", 14.35,
+              "kitchen", 15.0, 
+              "living room", 19.0,
+              "bedroom", 12.5,
+              "bathroom", 8.75,
+              "garage", 20.0,
+              "garden", 17.0 ]
+
+# Delete garage data
+_____________________
+
+# Print out all_areas
+print(all_areas)
---
+++ exercises/test_B9.py
@@ -0,0 +1,5 @@
+def test():
+    # Test
+    assert( all_areas == ['hallway', 14.35, 'kitchen', 15.0, 'living room', 19.0, 'bedroom', 12.5, 'bathroom', 8.75,'garden', 17.0]
+           ), "اجابة خاطئة: لم تقم بعملية الحذف بشكل صحيح"
+    __msg__.good("اجابة صحيحة. احسنت")
