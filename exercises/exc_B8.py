c640e050b70256aca375bca76f5af3fdac9aab81
dataXcode <smartvision789@gmail.com>
2020-10-06 12:20:44 +0300

    B8

---
+++ exercises/exc_B8.py
@@ -0,0 +1,12 @@
+# Areas list
+areas = [ "hallway", 14.35,
+          "kitchen", 15.0, 
+          "living room", 19.0,
+          "bedroom", 12.5,
+          "bathroom", 8.75 ]
+
+# Add garage and garden data
+all_areas = _______________________________________
+
+# Print out all_areas
+print(all_areas)
---
+++ exercises/test_B8.py
@@ -0,0 +1,5 @@
+def test():
+    # Test
+    assert( all_areas == ['hallway', 14.35, 'kitchen', 15.0, 'living room', 19.0, 'bedroom', 12.5, 'bathroom', 8.75, 'garage', 20.0, 'garden', 17.0]
+           ), "اجابة خاطئة: لم تقم بإنشاء قائمة جميع المساحات بشكل صحيح"
+    __msg__.good("اجابة صحيحة. احسنت")
