4030f95a8796fef950bf7c141996526dedb46238
dataXcode <smartvision789@gmail.com>
2020-10-06 21:13:51 +0300

    B10

--- exercises/test_B10.py
+++ exercises/test_B10.py
@@ -5,4 +5,10 @@ def test():
 
     assert( areas == ['hallway', 14.35, 'kitchen', 15.0, 'living room', 19.0, 'bedroom', 12.5, 'bathroom', 8.75]
            ), "اجابة خاطئة: لقد تأثرت قائمة المساحات الاصلية بالتعديل "
+
+    assert("print(areas_copy)" in __solution__ or "print( areas_copy )" in __solution__ or "print(areas_copy )" in __solution__ or "print( areas_copy)" in __solution__
+           ), "اجابة خاطئة: لم تقم بطباعة القائمة الجديدة بشكل صحيح"
+
+    assert("print(areas)" in __solution__ or "print( areas )" in __solution__ or "print(areas )" in __solution__ or "print( areas)" in __solution__
+           ), "اجابة خاطئة: لم تقم بطباعة قائمة المساحات الاصلية بشكل صحيح"
     __msg__.good("اجابة صحيحة. احسنت")
