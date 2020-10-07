ddef3be29d9b1c382d92b5ae2eae7a2786cb2edc
dataXcode <smartvision789@gmail.com>
2020-10-07 23:31:18 +0300

    C2

--- exercises/exc_C2.py
+++ exercises/exc_C2.py
@@ -3,7 +3,7 @@ name = "yaseen"
 #1 capitalize name
 print( ______________________ )
 
-#2 count number of e's in name
+#2 count number of "e" in name
 print( ______________________ )
 
 #3 replace "e" in name with "i"
--- exercises/solution_C2.py
+++ exercises/solution_C2.py
@@ -3,7 +3,7 @@ name = "yaseen"
 #1 capitalize name
 print( name.capitalize() )
 
-#2 count number of e's in name
+#2 count number of "e" in name
 print( name.count("e") )
 
 #3 replace "e" in name with "i"
