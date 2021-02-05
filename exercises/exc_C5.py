e7fb5969be6ac25a7f72628e1c76d770de6f0000
dataXcode <smartvision789@gmail.com>
2021-02-05 17:37:51 +0300

    C5

---
+++ exercises/exc_C5.py
@@ -0,0 +1,11 @@
+# Radius
+r = 0.4
+
+# Import math module
+______ ____
+
+#2 Calculate A
+A = ______________
+
+# Print out A
+print("Area: " , A)
---
+++ exercises/solution_C5.py
@@ -0,0 +1,11 @@
+# Radius
+r = 0.4
+
+# Import math module
+import math
+
+#2 Calculate A
+A = math.pi * r**2
+
+# Print out A
+print("Area: " , A)
