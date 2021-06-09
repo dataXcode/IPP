542906b732bad6f3346fe07ea890a7e17132aefb
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-06-09 13:43:03 +0300

    Create exc_D3.py

---
+++ exercises/exc_D3.py
@@ -0,0 +1,17 @@
+# Countries dictionary 
+countries = {"Iraq" : "Baghdad", "Egypt" : "Cairo", "Jordan" : "Amman", "Saudi Arabia" : "Riyadh"}
+
+#1 Check if Lebanon exist in countries
+print(_________ __ _________)
+
+#2 Add Lebanon : Beirut to countries
+countries[_________] = ________
+print(countries)
+
+#3 Delete Lebanon from countries
+___(countries[_________])
+print(countries)
+
+#4 Edit Iraq capital (Basra instead of Baghdad)
+countries[______] = _______
+print(countries)
