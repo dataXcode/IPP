c5236f1d62ea5dc41e3b42d26baf48f68499e865
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-06-09 14:19:01 +0300

    Create exc_D4.py

---
+++ exercises/exc_D4.py
@@ -0,0 +1,14 @@
+# Students dictionary
+students = {"Ahmed" : 87, "Waleed" : 69, "Hesham" : 92}
+
+# Khaled grades
+kh_grades = {"Math": 86, "English": 74}
+
+#1 Add Khaled and his grades to students
+students[________] = _________
+
+# Print out students
+print(students)
+
+#2 Print out Khaled grade in English
+print(_____________________________)
