cfd064b070b59aed71f0279f7c8fed6825411501
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-06-11 17:16:01 +0300

    Create exc_D6.py

---
+++ exercises/exc_D6.py
@@ -0,0 +1,18 @@
+school_friends = {"Ali", "Sara", "Waleed", "Yasser"}
+best_friends = {"Waleed", "Khaled", "Nor", "Ali"}
+
+#1 Add Ahmed to best_friends
+_________________________
+print(best_friends)
+
+#2 Discard Yasser from school_friends 
+________________________________
+print(school_friends)
+
+#3 Intersection of school_friends and best_friends
+int_friends = _____________________________
+print(int_friends)
+
+#4 Union of school_friends and best_friends
+un_friends = ______________________________
+print(un_friends)
