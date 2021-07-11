0531ff6406bcb74e1137a2bedb4625b778461442
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-07-11 09:10:17 +0300

    Create exc_E1.py

---
+++ exercises/exc_E1.py
@@ -0,0 +1,15 @@
+#1 Comparison of booleans
+print(__________________)
+
+#2 Comparison of integers
+print(__________________)
+
+#3 Comparison of strings
+print(__________________)
+
+#4 Compare a boolean with a numeric
+print(__________________)
+
+#5 Comparison of integers
+x = -7 * 3
+print(__________________)
