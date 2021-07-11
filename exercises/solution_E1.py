d0644f8c818fcef16b0191a72a9c57ce19a50bff
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-07-11 09:11:03 +0300

    Create solution_E1.py

---
+++ exercises/solution_E1.py
@@ -0,0 +1,15 @@
+#1 Comparison of booleans
+print(True == False)
+
+#2 Comparison of integers
+print(-2*10 != 20)
+
+#3 Comparison of strings
+print("python" == "Python")
+
+#4 Compare a boolean with a numeric
+print(True == 1)
+
+#5 Comparison of integers
+x = -7 * 3
+print(x >= -20)
