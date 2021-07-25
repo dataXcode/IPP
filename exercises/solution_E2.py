39ba8d0b44fbe2c62e8badf6148ff72f7eacab6d
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-07-25 09:57:18 +0300

    Create solution_E2.py

---
+++ exercises/solution_E2.py
@@ -0,0 +1,8 @@
+my_room = 22.0
+your_room = 16.0
+
+#1 my_room bigger than 12 and smaller than 25?
+print(my_room > 12 and my_room < 25)
+
+#2 Double my_room bigger than triple your_room?
+print(my_room * 2 > your_room * 3)
