0eb5cc650542e3e68f557f03c3655cb1983194d1
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-07-25 09:56:38 +0300

    Create exc_E2.py

---
+++ exercises/exc_E2.py
@@ -0,0 +1,8 @@
+my_room = 22.0
+your_room = 16.0
+
+#1 my_room bigger than 12 and smaller than 25?
+print(_____________________________)
+
+#2 Double my_room bigger than triple your_room?
+print(_____________________________)
