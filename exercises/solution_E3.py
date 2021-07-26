91f6c80d0bd64537584f38fc8744d59397c2e5da
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-07-26 15:38:21 +0300

    Create solution_E3.py

---
+++ exercises/solution_E3.py
@@ -0,0 +1,8 @@
+room = "bed"
+
+if room == "kit":
+  print("looking around in the kitchen.")
+elif room == "bed":
+  print("looking around in the bedroom.")
+else:
+  print("looking around elsewhere.")
