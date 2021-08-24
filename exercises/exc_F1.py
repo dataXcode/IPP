d202b16ab91150663c0451abef6b675edecab80b
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-08-24 12:06:34 +0300

    Create exc_F1.py

---
+++ exercises/exc_F1.py
@@ -0,0 +1,17 @@
+# Initialize counter = -4
+counter = -4
+
+#1 Code the while loop
+__________________:
+  #2 Print Great job
+  __________________
+  #3 if counter positive
+  ______________:
+    # Increase counter
+    _____________________
+  #4 else (negative)
+  ____:
+    # Decrease counter
+    _____________________
+  #5 Print counter
+  ______________
