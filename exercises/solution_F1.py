3e397c854caf5e5162dd50e68f82ffe01132587b
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-08-24 12:07:12 +0300

    Create solution_F1.py

---
+++ exercises/solution_F1.py
@@ -0,0 +1,17 @@
+# Initialize counter = -4
+counter = -4
+
+#1 Code the while loop
+while counter != 0:
+  #2 Print Great job
+  print("Great job")
+  #3 if counter positive
+  if counter > 0:
+    # Increase counter
+    counter = counter - 1
+  #4 else (negative)
+  else:
+    # Decrease counter
+    counter = counter + 1
+  #5 Print counter
+  print(counter)
