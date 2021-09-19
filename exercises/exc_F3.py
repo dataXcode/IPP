1b40e20de67dd4033fd35dbe17f27fcc60db67e3
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-19 13:53:38 +0300

    Update exc_F3.py

--- exercises/exc_F3.py
+++ exercises/exc_F3.py
@@ -2,4 +2,4 @@ areas = [14.35, 15.0, 19.0, 12.5, 8.75]
 
 # Code the for loop
 for index, area in ________________:
-  print('Room ' + str(_________) + ': ' + str(_________))
+  print('Room ' + ___(_________) + ': ' + ___(_________))
