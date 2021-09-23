b987a087e094e2a5acc85dfcdd1514331f1080ae
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-23 11:28:59 +0300

    Create exc_F4.py

---
+++ exercises/exc_F4.py
@@ -0,0 +1,9 @@
+house = [   ['hallway', 14.35],
+            ['kitchen', 15.0],
+            ['living room', 19.0],
+            ['bedroom', 12.5],
+            ['bathroom', 8.75]  ]
+
+# Code the for loop
+for x in _____:
+  print(___(____) + ' area is ' + ___(____) + 'm')
