1c3b07eba3a248e74b9a390a80874471a11e2687
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-23 12:05:59 +0300

    Create solution_F5.py

---
+++ exercises/solution_F5.py
@@ -0,0 +1,6 @@
+rooms = ['hallway','kitchen', 'living room', 'bedroom','bathroom']
+areas = [14.35, 15.0, 19.0, 12.5, 8.75]
+
+# Code the for loop
+for room, area in zip(rooms, areas):
+  print(str(room) + ' area is ' + str(area) + 'm')
