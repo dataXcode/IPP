b9c97583074aea2542d48f5c0dfcf1e5f3df6c27
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-23 11:29:37 +0300

    Create solution_F4.py

---
+++ exercises/solution_F4.py
@@ -0,0 +1,9 @@
+house = [   ['hallway', 14.35],
+            ['kitchen', 15.0],
+            ['living room', 19.0],
+            ['bedroom', 12.5],
+            ['bathroom', 8.75]  ]
+
+# Code the for loop
+for x in house:
+  print(str(x[0]) + ' area is ' + str(x[1]) + 'm')
