983fe99682fadda4f72d9a7cecc3acfe913bfd5c
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-26 14:43:35 +0300

    Create solution_F6.py

---
+++ exercises/solution_F6.py
@@ -0,0 +1,6 @@
+countries = {'iraq' : 'baghdad', 'egypt' : 'cairo',
+                'jordan' : 'amman', 'saudi arabia' : 'riyadh'}
+
+# Iterate over countries
+for key, value in countries.items():
+  print('the capital of ' + str(key) + ' is ' + str(value))
