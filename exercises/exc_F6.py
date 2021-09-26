6763b9c0a818e80c10e9b96e198d8ac39f48260a
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-26 14:43:03 +0300

    Create exc_F6.py

---
+++ exercises/exc_F6.py
@@ -0,0 +1,6 @@
+countries = {'iraq' : 'baghdad', 'egypt' : 'cairo',
+                'jordan' : 'amman', 'saudi arabia' : 'riyadh'}
+
+# Iterate over countries
+for key, value in _________________:
+  print('the capital of ' + ________ + ' is ' + __________)
