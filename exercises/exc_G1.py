fe0f2949991e37d91312142351193480c412206d
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-28 11:15:08 +0300

    Create exc_G1.py

---
+++ exercises/exc_G1.py
@@ -0,0 +1,10 @@
+num_list = [10,50,30,12,6,8,100]
+
+def max_min_first_last(nlist):
+    ____________________ # Maximum number
+    ____________________ # Minimum number
+    ____________________ # First number
+    ____________________ # Last number
+    return _____________________________________
+
+max_min_first_last(num_list)
