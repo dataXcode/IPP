0ed3800d8b93d1ed8d5b7cd779f95979a3677709
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-29 09:50:36 +0300

    Create solution_G2.py

---
+++ exercises/solution_G2.py
@@ -0,0 +1,9 @@
+celsius = 37.5
+
+# Create Tconverter function
+def Tconverter(celsius):
+  fahrenheit = (celsius * 1.8) + 32
+  return fahrenheit
+
+# Convert celsius = 37.5 to fahrenheit using Tconverter
+print( Tconverter(celsius) )
