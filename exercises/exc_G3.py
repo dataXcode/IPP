be24e6e9c0c1a40ee14204fb0ccfdab2fc82b7bb
dataXcode <63473696+dataXcode@users.noreply.github.com>
2021-09-29 11:56:27 +0300

    Create exc_G3.py

---
+++ exercises/exc_G3.py
@@ -0,0 +1,18 @@
+# define the Vehicle class
+class Vehicle:
+    
+  def __init__(self, name, color, price):
+    self.name = name
+    self.color = color
+    self.price = price
+        
+  def description(self):
+    return 'Car name: {} Color: {} Price: {}'.format(self.name, self.color, self.price)
+
+# Create car1 object and print out the description
+___________________________________
+___________________________________
+
+# Create car2 object and print out the description
+___________________________________
+___________________________________
