37c00f3c18925bdd792afd9b6313e337fe26d3fd
dataXcode <63473696+dataXcode@users.noreply.github.com>
2022-09-04 21:34:52 +0300

    Update test_A000.py

--- exercises/test_A000.py
+++ exercises/test_A000.py
@@ -1,6 +1,6 @@
 def test():
     # Test
-    assert("'hello world'" in __solution__ or "'Hello World'" in __solution__ or "'Hello world'" in __solution__ or "'HELLO WORLD'" in __solution__
-           or '"hello world"' in __solution__ or '"Hello World"' in __solution__ or '"Hello world"' in __solution__ or '"HELLO WORLD"' in __solution__
-           ), "اجابة خاطئة: لم تقم بطباعة الجملة المطلوبة بشكل صحيح"
+    assert("print('مرحباً بك')" in __solution__ or "print('مرحبا بك')" in __solution__ or "print( 'مرحباً بك')" in __solution__ or "print('مرحباً بك' )" in __solution__ or "print( 'مرحباً بك' )" in __solution__
+           or 'print("مرحباً بك")' in __solution__ or 'print("مرحبا بك")' in __solution__ or 'print( "مرحباً بك")' in __solution__ or 'print("مرحباً بك" )' in __solution__ or 'print( "مرحباً بك" )' in __solution__
+           ), "اجابة خاطئة: لم تقم بطباعة الجملة التي في الاعلى بشكل صحيح"
     __msg__.good("اجابة صحيحة. احسنت")
